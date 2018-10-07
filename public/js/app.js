b2239ab2157a098763ed19781d4fc84b1f05448e
Ta Hanh <tathimyhanh03@gmail.com>
2018-10-07 15:26:46 +0700

    welcome page

--- public/js/app.js
+++ public/js/app.js
@@ -963,7 +963,8 @@ module.exports = Cancel;
 /***/ (function(module, exports, __webpack_require__) {
 
 __webpack_require__(10);
-module.exports = __webpack_require__(43);
+__webpack_require__(43);
+module.exports = __webpack_require__(44);
 
 
 /***/ }),
@@ -47066,6 +47067,12 @@ if (false) {
 
 /***/ }),
 /* 43 */
+/***/ (function(module, exports) {
+
+// removed by extract-text-webpack-plugin
+
+/***/ }),
+/* 44 */
 /***/ (function(module, exports) {
 
 // removed by extract-text-webpack-plugin
