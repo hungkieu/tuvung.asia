cfc0e6f7dc3dcbeabb9599706411c766021d660d
nguyenthithuthao <thaothung.04@gmail.com>
2018-11-05 11:44:46 +0700

    edit reminder

--- resources/assets/js/components/setting/reminder.vue
+++ resources/assets/js/components/setting/reminder.vue
@@ -4,13 +4,13 @@
       <h6 class="py-2">Thời gian học tập</h6>
         <div class="uk-margin uk-grid-small uk-child-width-auto uk-grid">
           <span v-for="item in week" :key="item.key">
-            <input class="uk-checkbox" type="checkbox" name="days" v-model="checkedDaysWeek"
-            :checked="item.status ? true : false" value="item.key"   @change="item.status = !item.status" > {{item.name}}
+            <input class="uk-checkbox" type="checkbox" name="days" v-model="checkedDaysWeek" :value="item.key"> {{item.name}}
           </span>
         </div>
-        <button class="btn btn-primary" name="btn-plan" @click="save_plan">Lưu</button>
-      </form>
+       
       <p class="w-100">Vào lúc <input name="time" type="time" class="uk-input  uk-form-small w-25 mx-2  "  v-model="time"/> hằng ngày</p>
+       <button class="btn btn-primary" name="btn-plan" @click="save_plan">Lưu</button>
+      </form> 
   </div>
 </template>
 <script>
@@ -19,47 +19,55 @@
       return {
         time: {},
         checkedDaysWeek: [],
+        user: {},
         week: [
           {
             id: 'monday',
             name: 'Thứ 2',
-            status: true
+            status: true,
+            key:0
           },
           {
             id: 'tuesday',
             name: 'Thứ 3',
-            status: false
+            status: false,
+            key:1
           },
           {
             id: 'wednesday',
             name: 'Thứ 4',
-            status: false
+            status: false,
+            key:2
           },
           {
             id: 'thursday',
             name: 'Thứ 5',
-            status: false
+            status: false,
+            key:3
           },
           {
             id: 'friday	',
             name: 'Thứ 6',
-            status: false
+            status: false,
+            key:4
           },
           {
             id: 'saturday',
             name: 'Thứ 7',
-            status: false
+            status: false,
+            key:5
           },
           {
             id: 'sunday',
             name: 'Chủ nhật ',
-            status: false
+            status: false,
+            key:6
           }
         ],
         time: '21:00',
         }
         },
-        create(){
+        created(){
           this.user = Laravel.user;
           axios
             .get('/setting/reminder')
